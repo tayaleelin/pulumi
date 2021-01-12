e1ff62f65c4c217860792b734ec8f2ed712f820e
tayalee <tayalee@gmail.com>
2021-01-12 01:01:34 +0100

    Add initial pulumi stack for Aks deployment

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# pulumi
+# Pulumi
+
 Infrastructure- and Configuration as Code using Pulumi
+
+## Azure
+
+A project created scaffolded with Pulumi and advanced to create and configure Azure Resources
---
+++ azure/AksStack.cs
@@ -0,0 +1,21 @@
+using Pulumi;
+using Pulumi.AzureAD;
+using Pulumi.Azure.ContainerService;
+using Pulumi.Azure.ContainerService.Inputs;
+using Pulumi.Azure.Core;
+using Pulumi.Azure.Network;
+using Pulumi.Azure.Authorization;
+using Pulumi.Random;
+using Pulumi.Tls;
+
+class AksStack : Stack
+{
+  public AksStack()
+  {
+    var config = new Pulumi.Config();
+    var kubernetesVersion = config.Get("kubernetesVersion") ?? "1.19.1";
+
+    var resourceGroup = new ResourceGroup("aks")
+    }
+
+}
